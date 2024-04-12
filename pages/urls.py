8b42d228d6bb204d2cda8501318e6af26b41b313
Mohammadreza Asghary <ma3968312@gmail.com>
2024-04-12 12:35:38 +0330

    some changes(fix) in pages template

---
+++ pages/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import home_page_view, about_us_page_view, contact_us_page_view, faq_page_view, our_team_page_view, pricing_page_view, services_page_view, service_detail_page_view
+
+urlpatterns = [
+    path('', home_page_view, name='home'),
+    path('about_us/', about_us_page_view, name='about_us'),
+    path('contact_us/', contact_us_page_view, name='contact_us'),
+    path('faq/', faq_page_view, name='faq'),
+    path('our_team/', our_team_page_view, name='our_team'),
+    path('pricing/', pricing_page_view, name='pricing'),
+    path('services/', services_page_view, name='services'),
+    path('service_detail/', service_detail_page_view, name='service_detail'),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page_view(request):
+    return render(request, 'pages/home.html')
+
+def about_us_page_view(request):
+    return render(request, 'pages/about_us.html')
+
+def contact_us_page_view(request):
+    return render(request, 'pages/contact_us.html')
+
+def faq_page_view(request):
+    return render(request, 'pages/faq.html')
+
+def our_team_page_view(request):
+    return render(request, 'pages/our_team.html')
+
+def pricing_page_view(request):
+    return render(request, 'pages/pricing.html')
+
+def services_page_view(request):
+    return render(request, 'pages/services.html')
+
+def service_detail_page_view(request):
+    return render(request, 'pages/service_detail.html')
